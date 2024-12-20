767eeadb3d604ac4a0aa6b9eee793bfa6a0de8c5
YuuHanda <55708987+Yuuhanda@users.noreply.github.com>
2024-12-20 10:40:10 +0700

    feat ✨: Added database migration and initialization functionality with admin user creation.
    - Added database migration functionality with initial admin user creation.
    - Added database initialization and migration functionality to the main application.
    - Updated the executable file to reflect changes in the application.
    - Updated the binary executable file with new compiled code.

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	_ "rbac-beego-api/routers"
 
 	beego "github.com/beego/beego/v2/server/web"
+	"rbac-beego-api/database"
 )
 
 func main() {
+	database.InitDatabase()
+	database.RunMigrations()
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
